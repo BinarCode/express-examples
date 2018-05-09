11d4d6849e798fbf804665042dcf913c9d23928c
cristij <joracristi@gmail.com>
2018-05-09 16:49:04 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+## What is this?
+
+- A small collection of [express](https://expressjs.com/) based servers
+with various utility examples that you might want to use.
+
+## What's included ?
+
+- Small one file examples that are easy to follow with
+    - Authorization middleware based on roles
+    - Error handling middleware
+    - Translation support based on language headers
+    - Need more? Feel free to open an issue or PR
+
+## Prerequisites
+
+- Node.js > 8
+- Understanding of `async/await`/`promises`
---
+++ examples/authorization_middleware.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+
+const roles = {
+  SuperUser: 'super_user',
+  User: 'user',
+  Admin: 'admin',
+};
+
+/**
+ * Authorization middleware wrapped in a close so we can pass in additional data
+ * @param {array} roles accepted roles
+ * @return {Function} express middleware
+ */
+function authorize(roles = []){
+  return function(req, res, next) {
+
+    // Here you would access the role of the current user that you get from the database
+    let currentRole = req.query.role;
+
+    if(!roles.includes(currentRole)){
+      return res.status(401).send({message: 'You are not authorized to access this endpoint'})
+    }
+    return next();
+  }
+}
+/**
+ * call this with ?role=admin | ?role=user | ?role=super_user
+ * and you should receive 401 for ?role=user
+ */
+app.get('/', authorize([roles.Admin, roles.SuperUser]), function (req, res) {
+  res.status(200).send({message: `You accessed the endpoint with role ${req.query.role}`})
+});
+
+
+app.listen(3001, () => console.log('Authorization middleware example started'));
